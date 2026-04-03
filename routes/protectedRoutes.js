dbbd42636f34f53bc018f3cfdfb3b149f6de0ed0
Sarishti845 <sarishti260@gmail.com>
2026-04-04 01:07:55 +0530

    Updated userController logic

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -5,17 +5,17 @@ const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
 
 
 router.get('/admin', verifyToken, authorizeRoles('admin'), (req, res) => {
-  res.json({ message: "Welcome Admin 🔥" });
+  res.json({ message: "Welcome Admin " });
 });
 
 
 router.get('/analytics', verifyToken, authorizeRoles('admin', 'analyst'), (req, res) => {
-  res.json({ message: "Analytics Data 📊" });
+  res.json({ message: "Analytics Data " });
 });
 
 
 router.get('/dashboard', verifyToken, (req, res) => {
-  res.json({ message: "Dashboard Data 📈" });
+  res.json({ message: "Dashboard Data " });
 });
 
 module.exports = router;
